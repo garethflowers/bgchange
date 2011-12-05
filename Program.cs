18e1482e119ad81b3535d85a3d39592370e8f2d9
Gareth Flowers <garethflowers@users.noreply.github.com>
2011-12-05 01:13:42 +0000

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Gareth Flowers">
+//     Copyright Gareth Flowers. All rights reserved.
+// </copyright>
+// <summary>
+// Background Image Changer
+// </summary>
+//-----------------------------------------------------------------------
+
+namespace GarethFlowers.bgchange
+{
+    /// <summary>
+    /// Application main Class.
+    /// </summary>
+    public sealed class Program
+    {
+        /// <summary>
+        /// Prevents a default instance of the Program class from being created.
+        /// </summary>
+        private Program()
+        {
+            // n/a
+        }
+
+        /// <summary>
+        /// Main Method.
+        /// </summary>
+        /// <param name="args">Default command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+            if (!args.Length.Equals(1))
+            {
+                System.Console.WriteLine("ERROR: image location not defined");
+                return;
+            }
+
+            if (!System.IO.File.Exists(args[0]))
+            {
+                System.Console.WriteLine("ERROR: image file not found");
+                return;
+            }
+
+            using (Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop", true))
+            {
+                key.SetValue(@"WallpaperStyle", "1");
+                key.SetValue(@"TileWallpaper", "0");
+            }
+
+            NativeMethods.SetBackground(args[0]);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Gareth Flowers">
+//     Copyright Gareth Flowers. All rights reserved.
+// </copyright>
+// <summary>
+// Background Image Changer
+// </summary>
+//-----------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("bgchange")]
+[assembly: AssemblyDescription("Background Image Changer")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Gareth Flowers")]
+[assembly: AssemblyProduct("bgchange")]
+[assembly: AssemblyCopyright("Copyright © Gareth Flowers 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5ba49697-827b-4072-abff-7f0723af4146")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
