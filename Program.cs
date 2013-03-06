230d3a9547c55dc6df973406c0a77de2cd7af9bf
Gareth Flowers <garethflowers@users.noreply.github.com>
2013-03-06 10:34:34 +0000

    Added missing newline after IF block

--- Program.cs
+++ Program.cs
@@ -80,6 +80,7 @@ namespace GarethFlowers.BackgroundChanger
                         {
                             fileName = arg;
                         }
+
                         break;
                 }
             }
