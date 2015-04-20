a4127d8489e0cbed403e33d0e42ab61dfe1cf385
Gareth Flowers <garethflowers@users.noreply.github.com>
2015-04-20 10:34:38 +0100

    Override group policy registry settings

--- src/Program.cs
+++ src/Program.cs
@@ -87,9 +87,24 @@ namespace GarethFlowers.BGChange
 
             using (Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop", true))
             {
-                key.SetValue(@"WallpaperStyle", style.ToString(System.Globalization.CultureInfo.InvariantCulture));
-                key.SetValue(@"TileWallpaper", tile.ToString(System.Globalization.CultureInfo.InvariantCulture));
-                key.SetValue(@"Wallpaper", fileName);
+                key.SetValue("WallpaperStyle", style.ToString(System.Globalization.CultureInfo.InvariantCulture));
+                key.SetValue("TileWallpaper", tile.ToString(System.Globalization.CultureInfo.InvariantCulture));
+                key.SetValue("Wallpaper", fileName);
+            }
+
+            if (Security.IsUserAdministrator())
+            {
+                using (Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", true))
+                {
+                    if (key.GetValue("Wallpaper", null) != null)
+                    {
+                        key.SetValue("Wallpaper", fileName);
+                    }
+                    if (key.GetValue("WallpaperStyle", null) != null)
+                    {
+                        key.SetValue("WallpaperStyle", style.ToString(System.Globalization.CultureInfo.InvariantCulture));
+                    }
+                }
             }
 
             NativeMethods.SetBackground(fileName);
---
+++ src/Security.cs
@@ -0,0 +1,27 @@
+﻿namespace GarethFlowers.BGChange
+{
+    internal sealed class Security
+    {
+        public static bool IsUserAdministrator()
+        {
+            bool isAdmin;
+
+            try
+            {
+                System.Security.Principal.WindowsIdentity user = System.Security.Principal.WindowsIdentity.GetCurrent();
+                System.Security.Principal.WindowsPrincipal principal = new System.Security.Principal.WindowsPrincipal(user);
+                isAdmin = principal.IsInRole(System.Security.Principal.WindowsBuiltInRole.Administrator);
+            }
+            catch (System.Security.SecurityException)
+            {
+                isAdmin = false;
+            }
+            catch (System.UnauthorizedAccessException)
+            {
+                isAdmin = false;
+            }
+
+            return isAdmin;
+        }
+    }
+}
