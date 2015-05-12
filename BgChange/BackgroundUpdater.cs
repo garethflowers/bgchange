2a5044c3f2fae06d5b203f754d450d1d84295d4d
Gareth Flowers <garethflowers@users.noreply.github.com>
2015-05-12 23:31:25 +0100

    Separated core logic in BackgroundUpdater.cs

---
+++ BgChange/BackgroundUpdater.cs
@@ -0,0 +1,37 @@
+﻿
+namespace GarethFlowers.BGChange
+{
+    public sealed class BackgroundUpdater
+    {
+        private BackgroundUpdater()
+        {
+        }
+
+        public static void Apply(string fileName, int style, int tile)
+        {
+            using (Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop", true))
+            {
+                key.SetValue("WallpaperStyle", style.ToString(System.Globalization.CultureInfo.InvariantCulture));
+                key.SetValue("TileWallpaper", tile.ToString(System.Globalization.CultureInfo.InvariantCulture));
+                key.SetValue("Wallpaper", fileName);
+            }
+
+            if (Security.IsUserAdministrator())
+            {
+                using (Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", true))
+                {
+                    if (key.GetValue("Wallpaper", null) != null)
+                    {
+                        key.SetValue("Wallpaper", fileName);
+                    }
+                    if (key.GetValue("WallpaperStyle", null) != null)
+                    {
+                        key.SetValue("WallpaperStyle", style.ToString(System.Globalization.CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+
+            NativeMethods.SetBackground(fileName);
+        }
+    }
+}
--- BgChange/Program.cs
+++ BgChange/Program.cs
@@ -87,29 +87,7 @@ namespace GarethFlowers.BGChange
                 }
             }
 
-            using (Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop", true))
-            {
-                key.SetValue("WallpaperStyle", style.ToString(System.Globalization.CultureInfo.InvariantCulture));
-                key.SetValue("TileWallpaper", tile.ToString(System.Globalization.CultureInfo.InvariantCulture));
-                key.SetValue("Wallpaper", fileName);
-            }
-
-            if (Security.IsUserAdministrator())
-            {
-                using (Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System", true))
-                {
-                    if (key.GetValue("Wallpaper", null) != null)
-                    {
-                        key.SetValue("Wallpaper", fileName);
-                    }
-                    if (key.GetValue("WallpaperStyle", null) != null)
-                    {
-                        key.SetValue("WallpaperStyle", style.ToString(System.Globalization.CultureInfo.InvariantCulture));
-                    }
-                }
-            }
-
-            NativeMethods.SetBackground(fileName);
+            BackgroundUpdater.Apply(fileName, tile, style);
         }
     }
 }
