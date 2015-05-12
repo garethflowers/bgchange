8cb2fc68021eed2f05e4d11f2a30a9afa1139e53
Gareth Flowers <garethflowers@users.noreply.github.com>
2015-05-12 23:24:09 +0100

    Added private constructor to Security class

--- BgChange/Security.cs
+++ BgChange/Security.cs
@@ -2,6 +2,10 @@
 {
     internal sealed class Security
     {
+        private Security()
+        {
+        }
+
         public static bool IsUserAdministrator()
         {
             bool isAdmin;
